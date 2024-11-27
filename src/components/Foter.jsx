20d40b7a5c02f3269d79cb4c637165f8ca8961ac
amreshgupta <amreshgupta365@gmail.com>
2024-11-27 12:58:06 +0530

    new change

--- src/components/Foter.jsx
+++ src/components/Foter.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const Foter = () => {
   const date = new Date().getFullYear();
-  console.log(date);
   return (
     <div className="container-fluid bg-black my-3 footer1">
       <footer className="footer text-light">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,102 +7,102 @@ const Skills = () => {
       <div className=" container devlopment col-sm-3 col-md-4 col-lg-8">
         <div className="row align-items-center">
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/html.png"
-                class="card-img-top bg-light
+                className="card-img-top bg-light
                 "
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">HTML</h5>
+              <div className="card-body">
+                <h5 className="card-title">HTML</h5>
               </div>
             </div>
           </div>
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/css.png"
-                class="card-img-top bg-light"
+                className="card-img-top bg-light"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">CSS</h5>
+              <div className="card-body">
+                <h5 className="card-title">CSS</h5>
               </div>
             </div>
           </div>
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/javaScript.jpeg"
-                class="card-img-top jsimg"
+                className="card-img-top jsimg"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">JavaScript</h5>
+              <div className="card-body">
+                <h5 className="card-title">JavaScript</h5>
               </div>
             </div>
           </div>
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/reactLogo.png"
-                class="card-img-top"
+                className="card-img-top"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">React</h5>
+              <div className="card-body">
+                <h5 className="card-title">React</h5>
               </div>
             </div>
           </div>
         </div>
         <div className="row d-flex align-items-center my-3">
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/redux.webp "
-                class="card-img-top reduxImg"
+                className="card-img-top reduxImg"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">Redux</h5>
+              <div className="card-body">
+                <h5 className="card-title">Redux</h5>
               </div>
             </div>
           </div>
           <div className="col ">
-            <div class="card text-light bg-dark">
+            <div className="card text-light bg-dark">
               <img
                 src="src/assets/nodejs.webp"
-                class="card-img-top"
+                className="card-img-top"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">Node JS</h5>
+              <div className="card-body">
+                <h5 className="card-title">Node JS</h5>
               </div>
             </div>
           </div>
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/express.png"
-                class="card-img-top"
+                className="card-img-top"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">ExpessJs</h5>
+              <div className="card-body">
+                <h5 className="card-title">ExpessJs</h5>
               </div>
             </div>
           </div>
 
           <div className="col">
-            <div class="card bg-dark text-light">
+            <div className="card bg-dark text-light">
               <img
                 src="src/assets/mongodb.png"
-                class="card-img-top"
+                className="card-img-top"
                 alt="..."
               />
-              <div class="card-body">
-                <h5 class="card-title">MongoDB</h5>
+              <div className="card-body">
+                <h5 className="card-title">MongoDB</h5>
               </div>
             </div>
           </div>
